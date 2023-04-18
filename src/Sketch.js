8dad676aa75b670d227c47ca1ae7b299af33a164
misspia <pia_leung@hotmail.com>
2023-04-18 23:52:05 +0100

    initial commit

---
+++ src/Sketch.js
@@ -0,0 +1,16 @@
+import * as THREE from 'three'
+import { SketchManager } from './SketchManager'
+
+export class Sketch extends SketchManager {
+  constructor(canvas, audioElement) {
+    super(canvas, audioElement)
+  }
+
+  init() {
+
+  }
+
+  draw() {
+    
+  }
+}
---
+++ src/audio/meta.js
@@ -0,0 +1,6 @@
+export const BASE_URL = 'https://raw.githubusercontent.com/misspia/assets/master/audio/';
+
+export const audioMeta = {
+  url: `${BASE_URL}il-vento-doro.mp3`,
+  title: `Il vento d'oro - Yugo Kanno`,
+}
